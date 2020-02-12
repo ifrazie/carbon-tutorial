cab6f5a12d77fb570bfdcd8a03747c24e6e46080
ifrazie <ifrazie@wgu.edu>
2020-02-11 21:39:41 -0800

    feat(tutorial): complete step 3

---
+++ functions/Swapi_Api.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+
+const API_ENDPOINT = 'https://swapi.co/api/people/5/';
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data.name,
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
